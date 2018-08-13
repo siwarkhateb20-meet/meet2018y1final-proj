4bbe21c06596c4f37aaeac12f64d41889bd4a8c7
siwarkhateb20-meet <siwarkhateb20@meet.mit.edu>
2018-08-13 05:30:29 -0400

    Stopping here

---
+++ the q + a.py
@@ -0,0 +1,46 @@
+
+
+# the first question:
+
+answer = input("how many trees are cut down each day for toilet paper?, A = 50,000 , B = 20,250 , C = 27,000 , D = 150,000 ")
+if answer == "A" or answer == "a":
+    print( " you wish!")
+elif answer == "B" or answer == "b":
+    print(" optimistic haa?")
+elif answer == "C" or answer == "c" :
+    print(" finally , a real tree hugger")
+elif answer == "D" or answer == "d":
+    print(" wow, slow down champ")
+
+# the secound question:
+
+answer1 = input("how much time does it take for a glass bottle to decompose? A = 100 years, B = 4,000 years, C = 37,000 years, D = 2,500 years ")
+if answer1 == "A" or answer1 == "a":
+    print( " wake up! globale warming is real!")
+elif answer1 == "B" or answer1 == "b":
+    print(" thats right doggggggg")
+elif answer1 == "C" or answer1 == "c" :
+    print(" what the hell bro???")
+elif answer1 == "D" or answer1 == "d":
+    print(" sorry dude")
+
+#the third question:
+
+answer2 = input(" only ______% of our planet's water are fresh. A = 3%, B = 1%, c = 15%, D = 37% ")
+if answer2 == "A" or answer2 == "a":
+    print( " you get water! yay. you know, there are people in africa with no access to clean water, still happy? you monster!  ")
+elif answer2 == "B" or answer2 == "b":
+    print(" you cheap mother*#@$, you just killed half of the world population! ")
+elif answer2 == "C" or answer2 == "c" :
+    print(" have you seen the kineret??!")
+elif answer2 == "D" or answer2 == "d":
+    print(" you must be very stupid")
+
+# the forth question:
+
+answer3 = input(" 
+
+
+
+
+    
